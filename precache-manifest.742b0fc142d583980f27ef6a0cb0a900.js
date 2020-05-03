e3db4735bc2e482082be338a49ec88a1ac444e59
Dave Poulter <hello@davepoulter.net>
2020-05-03 15:45:19 +0100

    Adding build

---
+++ precache-manifest.742b0fc142d583980f27ef6a0cb0a900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5713907906265da8aa15b82c39fa69f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ddfd4034e1e18169e87",
+    "url": "/static/css/2.9f7154ce.chunk.css"
+  },
+  {
+    "revision": "515a5d985b79888f0a5c",
+    "url": "/static/css/main.cce216d4.chunk.css"
+  },
+  {
+    "revision": "1ddfd4034e1e18169e87",
+    "url": "/static/js/2.189ab7f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.189ab7f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "515a5d985b79888f0a5c",
+    "url": "/static/js/main.5853a351.chunk.js"
+  },
+  {
+    "revision": "aa40fb00a9870639ea34",
+    "url": "/static/js/runtime-main.21d18b49.js"
+  }
+]);
